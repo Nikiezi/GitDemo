38da52ae92d7a67894f9058c9d0c66935e32656c
lujiaming <lujiaming@tuniu.com>
2018-10-21 21:19:36 +0800

    master分支做了一些事，产生一次push

--- app/src/main/java/com/example/cookie/mygitdemo/MainActivity.java
+++ app/src/main/java/com/example/cookie/mygitdemo/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.cookie.mygitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tv);
+        //master分支添加一行注释
     }
 }
