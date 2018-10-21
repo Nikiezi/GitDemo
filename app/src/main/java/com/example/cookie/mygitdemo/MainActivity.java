eb1832fac4578239afe6d0cdac09aaa74c11eec9
lujiaming <lujiaming@tuniu.com>
2018-10-21 21:22:36 +0800

    dev1分支做了一些事情，产生了提交

--- app/src/main/java/com/example/cookie/mygitdemo/MainActivity.java
+++ app/src/main/java/com/example/cookie/mygitdemo/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.cookie.mygitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.tv);
+        imageView = findViewById(R.id.dev1_iv);
         //master分支添加一行注释
+        //dev1分支添加一行注释
     }
 }
